fc1be0e2e65a4498e0052a012a0c6bb9d95bba7a
Irakli_Imesashvili <imesashviliirakli5@gmail.com>
2024-04-20 04:24:01 +0400

    Add project files.

---
+++ SqlApp/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SqlApp.Models;
+
+public class Product
+{
+    [Key]
+    public int ProductID { get; set; }
+    public string ProductName { get; set; }
+
+    public int Quantity { get; set; }
+}
